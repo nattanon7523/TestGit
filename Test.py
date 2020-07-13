37b7ece6f9648bf3c15369d576204cf8200f607d
Gewvv <gew.nattanon@gmail.com>
2020-07-13 10:19:24 +0700

    Add py file
    Test

---
+++ Test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 26 11:02:43 2020
+
+@author: Gew
+"""
+print("10 + 2 = ",10+2)
+print("10 - 2 = ",10-2)
+print("10 * 2 = ",10*2)
+print("10 / 2 = ",10/2)
+
+v = int(input("Velocity : "))
+
+t = int(input("Time : "))
+
+result = v/t
+print("\nresult = ",result)
